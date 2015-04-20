a56cb3f8a4bf20d44da4a233e24e195d667f7531
Juan Carlos Palomo <juancarlos.palomo@gmail.com>
2015-04-20 19:33:15 +0200

    Different screen sizes and cache manifest

---
+++ screensize/js/cacheevents.js
@@ -0,0 +1,7 @@
+﻿function handleCacheEvent(e) {
+    alert(e.type);
+}
+
+applicationCache.addEventListener('cached', handleCacheEvent, false);
+applicationCache.addEventListener('checking', handleCacheEvent, false);
+
