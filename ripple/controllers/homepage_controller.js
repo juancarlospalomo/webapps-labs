4aaf89b655543325fe29387fef173159d3d15abb
juancarlospalomo <juancarlos.palomo@gmail.com>
2015-04-23 14:13:52 +0200

    UI feedback and FAB

---
+++ ripple/controllers/homepage_controller.js
@@ -0,0 +1,9 @@
+﻿var app = angular.module('homepageApp', ['ngMaterial']);
+
+app.controller('homepage_controller', function ($scope) {
+    $scope.line = 'Sample Text';
+    $scope.alert = function (value) {
+        alert(value);
+    }
+});
+
