e6c799e60c504122352f4d45eadd9a5f0ce2f92e
juancarlospalomo <juancarlos.palomo@gmail.com>
2015-04-23 10:35:04 +0200

    Tiles

---
+++ tiles/controllers/homepage_controller.js
@@ -0,0 +1,7 @@
+﻿var app = angular.module('homepageApp', ['ngMaterial']);
+
+app.controller('homepage_controller', function ($scope) { });
+
+app.config(function ($mdThemingProvider) {
+    $mdThemingProvider.theme('default').dark();
+});
